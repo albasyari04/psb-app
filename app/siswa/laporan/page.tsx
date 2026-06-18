1f07c2af9cd10212950f77841f119b25e12958e4
M. Naufal Jamil <albasyarialfaruq@gmail.com>
2026-06-18 13:24:06 +0700

    feat: Laporan Santri

---
+++ app/siswa/laporan/page.tsx
@@ -0,0 +1,8 @@
+export default function LaporanPage() {
+  return (
+    <div>
+      <h1>Halaman Laporan</h1>
+      <p>Konten untuk halaman laporan akan ditambahkan di sini.</p>
+    </div>
+  )
+}
